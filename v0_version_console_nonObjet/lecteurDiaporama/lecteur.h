2af0f8114c341d6d5ca9576356a28e01ba0d43db
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-04-05 16:49:34 +0200

    Ajout de : estVide, m_diaporama, charger(diaporama), getDiaporama

--- v0_version_console_nonObjet/lecteurDiaporama/lecteur.h
+++ v0_version_console_nonObjet/lecteurDiaporama/lecteur.h
@@ -6,7 +6,9 @@
 class Lecteur {
 private:
     bool m_estManuel;
+    bool m_estVide;
     unsigned short int m_vitesseDefilement; //Vitesse de défilement en secondes
+    Diaporama m_diaporama;
 
 public:
     Lecteur(bool=true, unsigned short int=2); //Le mode est manuel par défaut et la vitesse est de 2 secondes par défaut
@@ -14,9 +16,12 @@ public:
     ~Lecteur();
 
     bool getEstManuel(const Lecteur&) const;
+    bool getEstVide(const Lecteur&) const;
+    Diaporama getDiaporama(const Diaporama&) const;
     unsigned short int vitesseDefilement(const Lecteur&) const;
     void lireDiaporama(const Diaporama&) const;
     void arreterLecture(const Diaporama&) const;
+    void charger(const Diaporama&);
 
 
 
